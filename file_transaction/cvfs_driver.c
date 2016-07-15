75da415c0147a37b4f7ed95d31cf5103798a3743
int-argc <jeffrey_wong@dlsu.edu.ph>
2016-07-15 22:26:58 +0800

    driver initialize

--- file_transaction/cvfs_driver.c
+++ file_transaction/cvfs_driver.c
@@ -1,3 +1,6 @@
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <syslog.h>
 
@@ -9,24 +12,44 @@
 
 #define THREADCNT 2
 
-int main()
-{
-   pthread_t t[THREADCNT];
-   int i;
-   // open logging
-   openlog("cvfs2", LOG_PID|LOG_CONS, LOG_USER);
-system("clear");
- //  initialize();
-   while(1){
-      //pthread_create(&t[0], NULL, create_link, NULL);
-     pthread_create(&t[0], NULL, watch_temp, NULL);
-      pthread_create(&t[1], NULL, watch_share, NULL);
-      // pthread_create(&t[2], NULL, create_link, NULL);
+// shows help message and exit
+void show_help() {
+    printf("Usage:\n");
+    printf("\tcvfs_driver [init]\n");
+    printf("use init to initialize system");
+    exit(1);
+}
 
-      for (i = 0; i < THREADCNT; i++){
-         pthread_join(t[i], NULL);
-      }
-   }
+int main(int argc, char *argv[]) {
+    system("clear");
+    // open logging
+    openlog("cvfs2", LOG_PID|LOG_CONS, LOG_USER);
 
+    if(argc > 2){
+		printf("FATAL: Too much arguments.\n");
+        show_help();
+	} else if(argc == 2) {
+        if(strcmp(argv[1], "init") == 0) {      // initialize and exit
+            initialize();
+            exit(0);
+        } else {
+            printf("FATAL: Wrong argument.\n");
+    		show_help();
+        }
+    }
+
+    pthread_t t[THREADCNT];
+    int i;
+
+    while(1) {
+        //pthread_create(&t[0], NULL, create_link, NULL);
+        pthread_create(&t[0], NULL, watch_temp, NULL);
+        pthread_create(&t[1], NULL, watch_share, NULL);
+        // pthread_create(&t[2], NULL, create_link, NULL);
+
+        for (i = 0; i < THREADCNT; i++) {
+            pthread_join(t[i], NULL);
+        }
+    }
 
 }
